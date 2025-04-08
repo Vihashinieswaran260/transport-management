90cbbc93dfbd7346294e34b806d0e0e4daa6452f
Vihashini <vihashinieswaran06@gmail.com>
2025-04-08 10:32:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginForm from "./components/LoginForm";
 import StudentDashboard from "./components/pages/StudentDashboard";
 import AdminDashboard from "./components/pages/AdminDashboard";
-import ApplyTransport from "./components/pages/ApplyTransport"; 
+import ApplyTransport from "./components/pages/ApplyTransport";
 import CancelTransport from "./components/pages/CancelTransport";
 import BusTrack from "./components/pages/BusTrack";
 import Announcement from "./components/pages/Announcement";
 import ApplyRequests from "./components/pages/ApplyRequests";
 import CancelRequests from "./components/pages/CancelRequests";
-
 
 function App() {
   return (
@@ -24,16 +23,15 @@ function App() {
         <Route path="/announcements" element={<Announcement />} />
         <Route path="/apply-requests" element={<ApplyRequests />} />
         <Route path="/cancel-requests" element={<CancelRequests />} />
-
-
-
       </Routes>
     </Router>
   );
 }
 
-
 export default App;
+
+
+
 
 
 
